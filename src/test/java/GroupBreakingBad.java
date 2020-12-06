dc0047b877696378f2ce93a6e6b3b93e4f1df732
PeterKuzyk <p.kuzyk@yahoo.com>
2020-12-06 14:13:27 -0600

    Falls positive. Problem With popup window, asking for donation for automation test.

--- src/test/java/GroupBreakingBad.java
+++ src/test/java/GroupBreakingBad.java
@@ -2,26 +2,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-import java.security.Key;
+
+
+import static org.testng.Assert.assertEquals;
 
 public class GroupBreakingBad extends BaseTest {
 
-    public WebDriverWait getWait() {
-        return new WebDriverWait(getDriver(), 10);
-
-    }
-
-    protected void waitForVisible(WebElement element) {
-        getWait().until(ExpectedConditions.visibilityOf(element));
-    }
 
     @Test
     public void nataliaVats() throws InterruptedException {
@@ -34,27 +24,23 @@ public class GroupBreakingBad extends BaseTest {
         Thread.sleep(3000);
 
         WebElement input = driver.findElement(By.xpath("//input[contains(@class, 'ph-a11y-location-box')]"));
-        Assert.assertEquals(input.getAttribute("placeholder"), "Enter City, State or Zip");
+        assertEquals(input.getAttribute("placeholder"), "Enter City, State or Zip");
     }
 
 
     @Test
-    public void searchFieldTestPK() throws InterruptedException {
+    public void searchFieldTestPK() {
 
         WebDriver driver = getDriver();
         Actions actions = new Actions(driver);
         driver.get("https://www.wikipedia.org");
-
-        WebElement language = driver.findElement(By.xpath("//strong[contains(text(),'English')]"));
+        driver.findElement(By.xpath("//strong[contains(text(),'English')]")).click();
         WebElement search = driver.findElement(By.xpath("//input[@id='searchInput']"));
 
-        language.click();
-        actions
-                .sendKeys(search, "Selenium Webdriver")
-                .sendKeys(Keys.ENTER)
-                .perform();
+        actions.moveToElement(search).sendKeys("Selenium WebDriver")
+                .sendKeys(Keys.ENTER).build().perform();
 
-        driver.quit();
+        assertEquals(getDriver().getTitle(), "Wikipedia, the free encyclopedia");
 
     }
 
